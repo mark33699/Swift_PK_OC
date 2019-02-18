21001705c58b2934dafdfa099c4335e594889b61
Mark@Office <mark.hsieh@moneybook.com.tw>
2019-02-18 11:52:09 +0800

    比到第四題

--- SwiftPkOc/OCViewController.m
+++ SwiftPkOc/OCViewController.m
@@ -9,23 +9,48 @@
 #import "OCViewController.h"
 
 @interface OCViewController ()
-
+@property (nonatomic, strong) NSString *str;
 @end
 
 @implementation OCViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
+    
+    //pk.001_巢狀多行註解[OC]
+    /*
     // Do any additional setup after loading the view.
+     */
+    
+    //pk.002_let與immutable的差異[OC]
+//    self.str = @"我是一個字串";
+//    self.str = @"我是另一個字串";
+    
+    //pk.003_整數跟小數不能相加[OC]
+//    NSInteger intOC = 3 + 1.1;
+    
+    //pk.004_關於if[OC]
+//    1. 條件可以不用加小括號
+//    2. 條件一定要Bool值
+//    3. 執行區塊就算只有一行也要花括號
+//    4. 比較字串是否相等用==就可以了, isEqual是===
+//    5. 不能用怪招只跑else
+    
+    //    NSString *string;
+    NSString *string = @"ABCD";
+//    if (0)
+//    if (1)
+    if([string isEqual:@"ABCD"])
+        NSLog(@"OC條件為真");
+//        NSLog(@"OC條件為真是真的");
+    else
+        NSLog(@"OC條件為假");
+    
+    {
+        NSLog(@"OC條件為假");
+    }
 }
 
-//pk.001_巢狀多行註解[OC]
-/*
- 
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
- 
-*/
 
 @end
--- SwiftPkOc/SwiftViewController.swift
+++ SwiftPkOc/SwiftViewController.swift
@@ -10,15 +10,51 @@ import UIKit
 
 class SwiftViewController: UIViewController
 {
+    let str = "我是一個字串"
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        
         //pk.001_巢狀多行註解[Swift]
         /*
          /*
-            // Do any additional setup after loading the view, typically from a nib.
+         // Do any additional setup after loading the view, typically from a nib.
          */
-        */
+         */
+        
+        //pk.002_let與immutable的差異[Swift]
+//        str = "我是另一個字串"
+        
+        //pk.003_整數跟小數不能相加[Swift]
+//        let int : Int = 3 + 1.1
+        
+        //pk.004_關於if[Swift]
+//        1. 條件可以不用加小括號
+//        2. 條件一定要Bool值
+//        3. 執行區塊就算只有一行也要花括號
+//        4. 比較字串是否相等用==就可以了, isEqual是===
+//        5. 不能用怪招只跑else
+        
+
+//        if (false)
+//            print("條件為真")
+//        else
+//            print("條件為假")
+            
+        let string = "ABCD"
+        if (string == "ABCD")
+        {
+            print("Swift條件為真")
+        }
+        else
+        {
+            print("Swift條件為假")
+        }
+        
+//        {
+//            print("Swift條件為假")
+//        }
     }
 }
 
